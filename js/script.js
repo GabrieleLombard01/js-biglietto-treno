bfb8551bd1a259367f36d238c581e445007f336e
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-05-26 16:42:29 +0200

    price add

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ console.log('JS OK')
 /*
 0. Prendiamo un elemento nel DOM
 1. Chiedere all'utente il numero di kilometri che vuole percorrere
-2. Calcolare il prezzo del biglietto e salvarlo in una variabile 
-3. Chiedere all'utente l'età del passeggero 
+2. Chiedere all'utente l'età del passeggero 
+3. Calcolare il prezzo del biglietto e salvarlo in una variabile 
 4. Se l'utente ha anni da 0 a 18 (compresi), sottrarre -20% al prezzo biglietto
 5. Se l'utente ha anni da 65 a max (compresi), sottrarre -40% al prezzo biglietto
 6. Stampare il prezzo finale sull'elemento del DOM
@@ -15,7 +15,32 @@ const element = document.getElementById('target')
 
 //1. Chiedere all'utente il numero di kilometri che vuole percorrere
 const userKm = parseInt(prompt('Scegli i km che vuoi percorrere', '10', 500));
-
 console.log('km utente ' + userKm);
 
-//2. Calcolare il prezzo del biglietto e salvarlo in una variabile 
+//2. Chiedere all'utente il numero dei suoi anni
+const userAge = parseInt(prompt("Quanti anni hai?", 33).trim());
+console.log('età utente ' + userAge);
+
+//3. Calcolare il prezzo del biglietto e salvarlo in una variabile 
+const priceMoltiplicator = 0.21;
+
+let price = priceMoltiplicator * userKm;
+console.log(priceMoltiplicator, price);
+
+//4. Se l'utente ha anni da 0 a 18 (compresi), sottrarre -20% al prezzo biglietto
+//  5. Se l'utente ha anni da 65 a max (compresi), sottrarre -40% al prezzo biglietto
+const youngPeople = price * 0.2;
+const oldPeople = price * 0.4;
+
+console.log(youngPeople, oldPeople);
+
+if (userAge < 18){
+    price += - youngPeople;
+} else if (userAge > 64) {
+    price += - oldPeople;
+}
+
+console.log(price);
+
+//6. Stampare il prezzo finale sull'elemento del DOM
+
